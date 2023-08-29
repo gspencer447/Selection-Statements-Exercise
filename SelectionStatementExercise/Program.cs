3cdf9c2dfbd00732f3c306ba1c3ce45a3a4697f0
Graham Spencer <gspencer447@yahoo.com>
2023-08-29 11:03:59 -0500

    completed all steps for the exercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Guess my favorite number between 1 and 20.");
+            var userInput = int.Parse(Console.ReadLine());
+            int favoriteNumber = 13;
+
+            if (favoriteNumber > userInput)
+            {
+                Console.WriteLine("Your guess was too low.");
+            }
+
+            else if (favoriteNumber < userInput)
+            {
+                Console.WriteLine("Your guess was too high.");
+            }
+
+            else
+            {
+                Console.WriteLine("Congratulations, you guessed it!!!");
+            }
         }
     }
 }
